783cc9b14e47a605b326f49a8564f850af3b1c2a
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-16 09:50:04 -0400

    add file
    cycle2_lab 3

---
+++ lab_2-2.py
@@ -0,0 +1,11 @@
+# author: LM (AMDG) 09/15/2021
+a = 3
+b = a
+print(b)
+print(a)
+
+a=7
+# only a gets updated
+# if you want a to equal 7 put b = a
+print(a)
+print(b)
---
+++ lab_3-1.py
@@ -0,0 +1,22 @@
+# author: LM (AMDG) 09/16/2021
+
+a = 2 
+
+print(type(a))
+
+print(type(a) == int)
+
+
+a = float(a)
+print(type(a))
+
+a = str(a)
+print(type(a))
+
+a = bool(a)
+print(a == 2)
+
+# comparing true to two 
+# comparing booleanvalue of true to two. they are not equal
+
+
