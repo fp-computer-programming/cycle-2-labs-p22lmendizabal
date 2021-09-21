2982835039e6cd2559c26369f42a6926602cda42
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-21 10:06:07 -0400

    add file
    lab6.1

---
+++ lab_6-1.py
@@ -0,0 +1,10 @@
+# author : LM (AMDG) 9/21/21
+
+
+first_name = input("Write your first name. ")
+
+last_name = input("Write your last name. ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
